e8c461dfe3beb26ffc2b890a257aa5f7d10019c4
huogiafei <huogiafei@163.com>
2016-05-13 11:24:04 +0800

    ch10

--- ch08_arrowFunction/ls1.js
+++ ch08_arrowFunction/ls1.js
@@ -12,9 +12,34 @@ while (i-- >= 3) {
 //demo2 `--> tend to`
 console.info('---------- demo2 ----------');
 var j = 1;
-while (j --> 1) {
+while (j-- > 1) { //if i--> 0 死循环
     console.log(j);
     j++;
 }
 
-//test
+//demo3 ES6 new arrow `=>`
+{
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+    //es5
+    var result1 = arr.filter(function (item) {
+        return item % 2 == 0;
+    });
+
+    //es6 less function ,return and brackets
+    var result2 = arr.filter(item => item%2 == 0);//or item => {return item%2 == 0}
+    console.log(result1);
+    console.log(result2);
+}
+
+//demo4 warn: '{}' is object or block?
+let arr = [1,2,3];
+var init = arr.map(item => ({"name":item})); //error : (item => {"name":item})
+console.log(init);
+
+//demo5 this in `=>`function
+//TODO
+
+
+
+
+
